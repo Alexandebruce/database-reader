4c3084a8aa0845a14dc6f9d83f062807233829ab
Alexander <alexanderbruce.ikgsu@gmail.com>
2023-07-15 08:51:46 -0700

    Test application for docker (#1)
    * commit
    
    * final version test program
    
    * postgres by name

---
+++ DatabaseReader/DatabaseReader/Program.cs
@@ -0,0 +1,51 @@
+﻿using Npgsql;
+using System;
+using System.Data;
+using System.Threading;
+
+namespace DatabaseReader
+{
+    internal class Program
+    {
+        private static readonly string connectionString = "Server=postgres;Port=5432;Database=postgres;User Id=postgres;SearchPath=test_schema;Password=mysecretpassword;";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's started");
+
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM test_table;";
+                using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+                {
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32("id");
+                            string name = reader.GetString("name");
+                            string surname = reader.GetString("surname");
+                            string patronymic = reader.GetString("patronymic");
+                            string comment = reader.GetString("comment");
+                            bool deleted = reader.GetBoolean("deleted");
+
+                            Console.WriteLine($"Readed user: Surname:{surname}, Name:{name}, Patronymic:{patronymic}, Comment:{comment}, Deleted:{deleted}");
+                        }
+                    }
+                }
+
+                connection.Close();
+            }
+
+            Console.WriteLine("Finished reading");
+
+            for (;;)
+            {
+                Console.WriteLine("It's alive");
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,16 @@ WORKDIR /app
 
 # Copy the solution file and restore dependencies
 COPY *.sln .
-COPY DatabaseReader/DatabaseReader.csproj ./DatabaseReader/
-RUN dotnet restore
+COPY DatabaseReader/DatabaseReader/DatabaseReader.csproj ./DatabaseReader/
+RUN dotnet restore ./DatabaseReader/DatabaseReader.csproj
 
 # Copy the entire project and build
 COPY . .
-RUN dotnet build -c Release --no-restore
+RUN dotnet build -c Release --no-restore ./DatabaseReader/DatabaseReader.csproj
 
 # Stage 2: Publish the application
 FROM build AS publish
-RUN dotnet publish -c Release --no-build -o /app/publish
+RUN dotnet publish ./DatabaseReader/DatabaseReader.csproj -c Release --no-build -o /app/publish
 
 # Stage 3: Create the final runtime image
 FROM mcr.microsoft.com/dotnet/runtime:7.0
